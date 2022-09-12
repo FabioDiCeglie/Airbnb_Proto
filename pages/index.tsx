f9faac00209c85341f91c0ecbc0a3de1327e1365
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-12 21:33:55 +0200

    cleaning initial code

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <h1 className="text-white">Ciao</h1>
+    </div>
+  );
+};
+
+export default Home;
