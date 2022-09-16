9c1e09a92b0c7e974c4b27e78546723a04cd9561
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-16 14:13:57 +0200

    feat: start to work on Banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 const Home: NextPage = () => {
@@ -8,10 +9,9 @@ const Home: NextPage = () => {
       <Head>
         <title>Airbnb</title>
       </Head>
-      <>
-        <Header />
-      </>
-      {/* Banner */}
+
+      <Header />
+      <Banner />
     </div>
   );
 };
