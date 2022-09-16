223139244d2786de9932b8f2895d0991a85b4dad
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-16 14:03:21 +0200

    feat: styling for the search bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,9 +13,13 @@ function Header() {
           objectPosition="left"
         />
       </div>
-      <div>
-        <input type="text" placeholder="Start your search" />
-        <SearchIcon className="h-8" />
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm ">
+        <input
+          type="text"
+          placeholder="Start your search"
+          className="flex-grow pl-5 bg-transparent outline-none"
+        />
+        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
       <div></div>
     </header>
