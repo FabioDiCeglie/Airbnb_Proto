d5c5c2ada8868bf16acf78435babdd5e3af55eab
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-16 14:10:42 +0200

    feat: working on left side of search bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/solid";
+import {
+  SearchIcon,
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UsersIcon,
+} from "@heroicons/react/solid";
 
 function Header() {
   return (
@@ -13,7 +19,8 @@ function Header() {
           objectPosition="left"
         />
       </div>
-      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm ">
+
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm text-gray-600 placeholder-gray-400">
         <input
           type="text"
           placeholder="Start your search"
@@ -21,7 +28,16 @@ function Header() {
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
-      <div></div>
+
+      <div className="flex items-center space-x-4 justify-end text-gray-500">
+        <p>Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+          <MenuIcon className="h-6 cursor-pointer" />
+          <UserCircleIcon className="h-6 cursor-pointer" />
+        </div>
+      </div>
     </header>
   );
 }
