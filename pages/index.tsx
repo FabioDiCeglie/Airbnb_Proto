4e958d73d57bdde2b3464b99b55cb9620efb53ce
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-18 10:54:22 +0200

    fix: fix the banner and start the main sect

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,12 @@ const Home: NextPage = () => {
 
       <Header />
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-3xl font-semibold pb-5">Explore Nearby</h2>
+        </section>
+      </main>
     </div>
   );
 };
