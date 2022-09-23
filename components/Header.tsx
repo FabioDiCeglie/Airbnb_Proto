55c614e28eb7ba5bccf36da97987afe22dac987e
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-23 14:37:51 +0200

    feat: working on the search input

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,11 +8,10 @@ import {
 import { useState } from "react";
 
 function Header() {
-  const [searchInput, setSearchInput] = useState();
+  const [searchInput, setSearchInput] = useState("");
 
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10">
-      {}
       <div className="relative flex items-center h-10 cursor-pointer">
         <Image
           src="https://links.papareact.com/qd3"
@@ -25,9 +24,11 @@ function Header() {
 
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm text-gray-600 placeholder-gray-400">
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           type="text"
           placeholder="Start your search"
-          className="flex-grow pl-5 bg-transparent outline-none"
+          className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
@@ -41,6 +42,8 @@ function Header() {
           <UserCircleIcon className="h-6 cursor-pointer" />
         </div>
       </div>
+
+      {searchInput && <div>Ciao</div>}
     </header>
   );
 }
