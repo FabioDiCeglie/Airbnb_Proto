3bff08eb6006a0a54ba85d7b15723a897e49caf6
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-26 21:16:45 +0200

    feat: adding ProgressBar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
 import "../styles/global.css";
 import type { AppProps } from "next/app";
+import ProgressBar from "@badrap/bar-of-progress";
+
+const progress = new ProgressBar({
+  size: 4,
+  color: "#FE595E",
+  className: "z-50",
+  delay: 100,
+});
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return <Component {...pageProps} />;
