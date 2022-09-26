2ad836220487d1ea3815dd6a7389d0acfb3b9170
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-26 21:08:19 +0200

    feat: showing price and total in InfoCard

--- components/InfoCard.tsx
+++ components/InfoCard.tsx
@@ -26,6 +26,20 @@ export default function InfoCard({
         <h4 className="text-xl">{title}</h4>
 
         <div className="border-b w-10 pt-2" />
+
+        <p className="pt-2 text-sm text-gray-500 flex-grow">{description}</p>
+
+        <div className="flex justify-between items-end pt-5">
+          <p className="flex items-center">
+            <StarIcon className="h-5 text-red-400" />
+            {star}
+          </p>
+
+          <div>
+            <p className="text-lg lg:text-2xl font-semibold pb-2">{price}</p>
+            <p className="text-right font-extralight">{total}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
